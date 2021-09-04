75daee56395cd5d0244171a38ffa2eabc82650a3
Ivan Butrimov <ivanbutrimov@gmail.com>
2021-09-04 15:36:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/water_nn/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.water_nn.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.water_nn.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
