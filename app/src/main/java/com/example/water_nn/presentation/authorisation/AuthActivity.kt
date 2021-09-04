a257896e2c42be51eba93098a8f18058c3516262
Ivan Butrimov <ivanbutrimov@gmail.com>
2021-09-04 15:49:02 +0300

    Create project

---
+++ app/src/main/java/com/example/water_nn/presentation/authorisation/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.water_nn.presentation.authorisation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.water_nn.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
---
+++ app/src/main/java/com/example/water_nn/presentation/authorisation/AuthFragment.kt
@@ -0,0 +1,18 @@
+package com.example.water_nn.presentation.authorisation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.water_nn.R
+
+class AuthFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_auth, container, false)
+    }
+}
